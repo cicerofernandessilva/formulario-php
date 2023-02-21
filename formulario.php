861fa611f72446aec29d8ed580390a16e91c3013
cicerofernandessilva <cicerofernandessilva@gmail.com>
2023-02-20 23:16:51 -0300

    Modificações para resolução de erros

--- formulario.php
+++ formulario.php
@@ -1,77 +1,75 @@
 <?php
 
-// echo json_encode('Hola PHP!');
-
-// isset — Informa se a variável foi iniciada
-// empty — Determina se a variável é vazia,
-// trim — Retira espaço no início e final de uma string
+// isset — Determina si una variable está definida y no es null
+// empty — Determina si una variable está vacía
+// trim — Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el final de la cadena
 
 if($_POST){
     $usuario = "";
     $email = "";
     $mensaje = "";
-    
+
     if(isset($_POST['usuario'])){
         $usuario = filter_var(trim($_POST['usuario']), FILTER_SANITIZE_STRING);
-        // echo json_encode($usuario);
-    };
+    }
     if(isset($_POST['email'])){
         $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
-        //echo json_encode($email);
-    };
+    }
     if(isset($_POST['mensaje'])){
         $mensaje = filter_var(trim($_POST['mensaje']), FILTER_SANITIZE_STRING);
-        // echo json_encode($mensaje);
-    };
+    }
 
     if(empty($usuario)){
         echo json_encode(array(
-            'erro' => true,
-            'campo' => 'usuario' 
+            'error' => true,
+            'campo' => 'usuario'
         ));
         return;
     }
+
     if(empty($email)){
         echo json_encode(array(
-            'erro' => true,
-            'campo' => 'email' 
+            'error' => true,
+            'campo' => 'email'
         ));
         return;
     }
+
     if(empty($mensaje)){
         echo json_encode(array(
-            'erro' => true,
-            'campo' => 'mensaje' 
+            'error' => true,
+            'campo' => 'mensaje'
         ));
         return;
     }
-    //cuerpo del mensaje
+
+    // Cuerpo del mensaje
     $cuerpo = 'Usuario: ' . $usuario . '<br>';
-    $cuerpo = 'Email: ' . $email . '<br>';
-    $cuerpo = 'Mensaje: ' . $mensaje . '<br>';
+    $cuerpo .= 'Email: ' . $email . '<br>';
+    $cuerpo .= 'Mensaje: ' . $mensaje . '<br>';
 
-    //direccion
-
-    $destinatario = 'cicerofernandessilva@gmail.com';
+    // Direccion
+    $destinatario = 'cicerofernandessilva@gmail.com'; //reemplazar por su email
     $asunto = 'Mensaje de mi sitio web';
     $headers  = 'MIME-Version: 1.0' . "\r\n" .'Content-type: text/html; charset=utf-8' . "\r\n" .'From: ' . $correo . "\r\n";
 
     if(mail($destinatario, $asunto, $cuerpo, $headers)){
         echo json_encode(array(
-            'erro' => false,
+            'error' => false,
             'campo' => 'exito'
         ));
     } else {
         echo json_encode(array(
-            'erro' => true,
+            'error' => true,
             'campo' => 'mail'
         ));
     }
+
+
 } else {
     echo json_encode(array(
-        'erro' => true,
-        'campo' => 'POST'
+        'error' => true,
+        'campo' => 'post'
     ));
 }
 
-?>
--- js/app.js
+++ js/app.js
@@ -1,18 +1,14 @@
-
 // console.log('Funciona!')
-
 const formulario = document.getElementById('formulario');
 const usuario = document.getElementById('texto-1');
 const email = document.getElementById('texto-2');
 const mensaje = document.getElementById('texto-3');
 const boton = document.getElementById('boton');
 
-
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
     e.stopPropagation();
     // console.log('Formulário funcionando') /* teste formulário*/
-
     const data = new FormData(formulario);
     // console.log(data.get('mensaje')); // teste de consta data
     const campoValido = (campo) => {
@@ -34,23 +30,23 @@ formulario.addEventListener('submit', (e) => {
     }
 
     if (!data.get('usuario').trim()) {
-         console.log('no hay usuario!')
-        campoInvalido(usuario)
-        return
+        console.log('no hay usuario!');
+        campoInvalido(usuario);
+        return;
     } else {
         campoValido(usuario);
     }
     if (!data.get('email').trim()) {
-        console.log('no hay email!')
-        campoInvalido(email)
-        return
+        console.log('no hay email!');
+        campoInvalido(email);
+        return;
     } else {
         campoValido(email);
     }
     if (!data.get('mensaje').trim()) {
-        console.log('no hay mensaje!')
-        campoInvalido(mensaje)
-        return
+        console.log('no hay mensaje!');
+        campoInvalido(mensaje);
+        return;
     } else {
         campoValido(mensaje);
     }
@@ -63,29 +59,28 @@ formulario.addEventListener('submit', (e) => {
         .then((res) => res.json())
         .then((datos) => {
             console.log(datos);
-
-            if (datos.erro && datos.campo === "usuario") {
+            if (datos.error && datos.campo === "usuario") {
                 console.log("erro con usuario PHP");
                 campoInvalido(usuario);
                 return;
-            } else {
-                campoValido(usuario);
             }
-            if (datos.erro && datos.campo === "email") {
+            campoValido(usuario);
+
+            if (datos.error && datos.campo === "email") {
                 console.log("erro con email PHP");
                 campoInvalido(email);
                 return;
-            } else {
-                campoValido(email);
             }
-            if (datos.erro && datos.campo === "mensaje") {
+            campoValido(email);
+
+            if (datos.error && datos.campo === "mensaje") {
                 console.log("erro con mensaje PHP");
                 campoInvalido(mensaje);
                 return;
-            } else {
-                campoValido(mensaje);
             }
-            if (!datos.erro) {
+            campoValido(mensaje);
+
+            if (!datos.error) {
                 limpiarFormulario();
                 campoValido(boton);
             }
